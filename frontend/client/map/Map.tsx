35e5ea045db5fe78a1c005492629b73662e3cf22
James Jeong <jamesjeong94@gmail.com>
2020-09-01 15:25:41 -0400

    added polyline

--- frontend/client/map/Map.tsx
+++ frontend/client/map/Map.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 
 type MapProps = {
@@ -7,20 +7,34 @@ type MapProps = {
 };
 
 const Map: React.FC<MapProps> = ({ getStopsForSubwayLine, subwayLine }) => {
+  const [map, setMap] = useState(null);
+  const [maps, setMaps] = useState(null);
+  const [markers, setMarkers] = useState(null);
+  const [mapLoaded, changeMapLoad] = useState(false);
+
   useEffect(() => {
     if (subwayLine !== '') {
       getStopsForSubwayLine(subwayLine);
     }
   }, [subwayLine]);
 
-  const map = (
+  const onMapLoaded = (map: any, maps: any) => {
+    setMap(map);
+    setMaps(maps);
+    changeMapLoad(true);
+  };
+
+  const GoogleMap = (
     <GoogleMapReact
       bootstrapURLKeys={{ key: process.env.GMapKey }}
       defaultCenter={{ lat: 40.7128, lng: -74.006 }}
       defaultZoom={12}
+      onGoogleApiLoaded={({ map, maps }: { map: any; maps: any }) =>
+        onMapLoaded(map, maps)
+      }
     ></GoogleMapReact>
   );
-  return <div style={{ height: '80vh', width: '100%' }}>{map}</div>;
+  return <div style={{ height: '80vh', width: '100%' }}>{GoogleMap}</div>;
 };
 
 export = Map;
---
+++ frontend/client/map/components/Polyline.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+
+interface PolylineProps {
+  markers: any;
+  map: any;
+  maps: any;
+}
+
+export const Polyline: React.FC<PolylineProps> = ({ markers, map, maps }) => {
+  const [polyline, setPolyline] = useState<any>(null);
+
+  const renderPolyline = () => {
+    if (polyline !== null) {
+      polyline.setMap(null);
+    }
+    let nonGeodesicPolyline = new maps.Polyline({
+      path: markers,
+      geodesic: false,
+      strokeColor: 'black',
+      strokeOpacity: 1,
+      strokeWeight: 5,
+    });
+    setPolyline(nonGeodesicPolyline);
+    nonGeodesicPolyline.setMap(map);
+
+    useEffect(() => {
+      renderPolyline();
+    }, [markers]);
+  };
+
+  return null;
+};
